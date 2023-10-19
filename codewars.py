8a506b9e225f49fab17be20fdab6e19f345c8115
bdperealon <perezalonsodamian@gmail.com>
2023-10-19 22:32:38 +0200

    fix filter_list for non-negative integers

--- codewars.py
+++ codewars.py
@@ -4,11 +4,12 @@ import re
 def disemvowel(string_):
     return re.sub("[AEIOUaeiou]", "", string_)
 
-
+# In this kata you will create a function that takes a list of non-negative integers and strings
+# and returns a new list with the strings filtered out.
 def filter_list(list):
     new_list = []
     for item in list:
-        if type(item) == type(1) and item > 0:
+        if type(item) == int and item >= 0:
             new_list.append(item)
     return new_list
 
@@ -16,7 +17,7 @@ def filter_list(list):
 def main():
     print("Hello World!")
     print(disemvowel("Hello World!"))
-    print(filter_list([-1, 1, "a", 2, -2, "b", "c", 3, -3]))
+    print(filter_list([-1, 1, "a", 2, -2, "b", "c", 3, -3, -555, 666]))
 
 
 if __name__ == "__main__":
