84ff3e4719af6e16f95fb79f56516946e5ca3561
bdperealon <perezalonsodamian@gmail.com>
2023-10-19 22:28:23 +0200

    codewars filter list function

--- codewars.py
+++ codewars.py
@@ -5,9 +5,19 @@ def disemvowel(string_):
     return re.sub("[AEIOUaeiou]", "", string_)
 
 
+def filter_list(list):
+    new_list = []
+    for item in list:
+        if type(item) == type(1) and item > 0:
+            new_list.append(item)
+    return new_list
+
+
 def main():
     print("Hello World!")
     print(disemvowel("Hello World!"))
+    print(filter_list([-1, 1, "a", 2, -2, "b", "c", 3, -3]))
+
 
 if __name__ == "__main__":
     main()
